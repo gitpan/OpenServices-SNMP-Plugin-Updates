af1114780193d4651adc186996bceb2cfc7d14e0
Michael Fladischer <fladi@cpan.org>
2014-10-30 09:48:41 +0000

    Import of FLADI/OpenServices-SNMP-Plugin-Updates-0.02 from CPAN.
    gitpan-cpan-distribution: OpenServices-SNMP-Plugin-Updates
    gitpan-cpan-version:      0.02
    gitpan-cpan-path:         FLADI/OpenServices-SNMP-Plugin-Updates-0.02.tar.gz
    gitpan-cpan-author:       FLADI
    gitpan-cpan-maturity:     released

--- lib/OpenServices/SNMP/Plugin/Updates.pm
+++ lib/OpenServices/SNMP/Plugin/Updates.pm
@@ -15,11 +15,11 @@ OpenServices::SNMP::Plugin::Updates - Expose pending security updates over SNMP
 
 =head1 VERSION
 
-Version 0.01
+Version 0.02
 
 =cut
 
-our $VERSION = '0.01';
+our $VERSION = '0.02';
 
 =head1 BASE OID
 
@@ -63,6 +63,8 @@ sub init {
     }
 
     printf STDERR "Registering %s handler.\n", __PACKAGE__;
+    # Prepopulate the cache.
+    check();
     $agent->register(__PACKAGE__, $BASEOID, \&handler);
 }
 
