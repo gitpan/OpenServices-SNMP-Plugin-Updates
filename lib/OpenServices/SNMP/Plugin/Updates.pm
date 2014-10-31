83112e921f2d93acf011ac331353a1b6e96b87e5
Michael Fladischer <fladi@cpan.org>
2014-10-31 08:18:31 +0000

    Import of FLADI/OpenServices-SNMP-Plugin-Updates-0.03 from CPAN.
    gitpan-cpan-distribution: OpenServices-SNMP-Plugin-Updates
    gitpan-cpan-version:      0.03
    gitpan-cpan-path:         FLADI/OpenServices-SNMP-Plugin-Updates-0.03.tar.gz
    gitpan-cpan-author:       FLADI
    gitpan-cpan-maturity:     released

--- lib/OpenServices/SNMP/Plugin/Updates.pm
+++ lib/OpenServices/SNMP/Plugin/Updates.pm
@@ -15,11 +15,11 @@ OpenServices::SNMP::Plugin::Updates - Expose pending security updates over SNMP
 
 =head1 VERSION
 
-Version 0.02
+Version 0.03
 
 =cut
 
-our $VERSION = '0.02';
+our $VERSION = '0.03';
 
 =head1 BASE OID
 
